89352530113e753202e90ecef0b009179ec313e0
Hazesz <ihenriquekogavigato@gmail.com>
2025-03-31 15:36:01 -0300

    correção

--- Aula2/ex1.cpp
+++ Aula2/ex1.cpp
@@ -3,20 +3,22 @@
 int main(){
     int matriz[3][4] = {};
     int soma = 0;
+    int line, colum;
 
-    for(int l = 0; l < 3; l++){
-        for(int c = 0; c < 4; c++){
-            std::cout << "Digite o elemento["<< l <<"]["<< c <<"] da matriz:";
-            std::cin >> matriz[l][c];
-        }
-    }
-
-    for(int line = 0; line < 3; line++){
+    for(line = 0; line < 3; line++){
         for(int colum = 0; colum < 4; colum++){
-            soma += matriz[line][colum];
+            std::cout << "Digite o elemento["<< line <<"]["<< colum <<"] da matriz:";
+            std::cin >> matriz[line][colum];
         }
     }
-    std::cout << "Soma dos valores:"<< soma <<"";
+    //ajustar pra fazer por coluna
+    for(colum = 0; colum < 4; colum++){
+        soma = 0;
+        for(line = 0; line < 3; line++){
+        soma += matriz[line][colum];
+        }
+        std::cout << "Soma dos valores da coluna "<<colum<<": "<< soma <<std::endl;
+    }
 
     return 0;
 }
