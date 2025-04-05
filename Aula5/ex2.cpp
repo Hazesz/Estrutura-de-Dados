6313ae2804b1b48c4d0f7d2d5bd958d64b4731ba
Hazesz <ihenriquekogavigato@gmail.com>
2025-04-05 16:10:04 -0300

    Ex 2 Completo

--- Aula5/ex2.cpp
+++ Aula5/ex2.cpp
@@ -6,29 +6,68 @@ struct no{
     no* ant;
 };
 
-void duplicados(no*& primeiro){
+void exibir(no *primeiro){
     no* atual = primeiro;
-    no* verificador;
-    while(atual != nullptr){
-        no* verificador = atual -> prox;
-        while(verificador != nullptr){
-            if(verificador == atual){
-                
-            }
-        }
+    while( atual != nullptr){
+        std::cout << atual -> valor << " ";
+        atual = atual -> prox;
     }
 }
 
-int main(){
-    no* primeiro = new no {10}; 
-    no* segundo = new no {20};
-    no* terceiro = new no {30};
-    no* quarto = new no {10};
+void duplicados(no*& primeiro){
+    no* atual = primeiro;
+    
+    while(atual != nullptr){
+        no* verificador = atual -> prox; //começa a procurar os valores duplicados após o valor atual
+        
+        while(verificador != nullptr){
+            no* prox = verificador -> prox; //guarda o proximo nó antes de deletar
+            
+            if(verificador -> valor == atual -> valor){
+                //remove o nó duplicado
+               if(verificador -> ant != nullptr){
+                verificador -> ant -> prox = verificador -> prox;
+               }
+                if(verificador -> prox != nullptr){
+                    verificador -> prox -> ant = verificador -> ant;
+                }
+               delete verificador; //libera mem do nó duplicado
+            }
+            verificador = prox; //continua verificando
+        }
+        atual = atual -> prox; //vai para o proximo da lista
+    }
+}
+//eu consigo entender perfeitamente a abstração desse processo, mas a implementação é realmente um desafio
 
-    primeiro -> ant = quarto;
+int main(){
+    no* primeiro = new no {10, nullptr, nullptr}; 
+    no* segundo = new no {20, nullptr, nullptr};
+    no* terceiro = new no {30, nullptr, nullptr};
+    no* quarto = new no {10, nullptr, nullptr};
+
+   
     primeiro -> prox = segundo;
+
+    segundo -> ant = primeiro;
     segundo -> prox = terceiro;
+
+    terceiro -> ant = segundo;
     terceiro -> prox = quarto;
 
+    quarto -> ant = terceiro;
+
+    std::cout << "Com nos duplicados:";
+    exibir(primeiro);
+    std::cout << "\n";
+
     duplicados(primeiro);
+
+    std::cout << "Sem nos duplicados:";
+    exibir(primeiro);
+
+    delete primeiro;
+    delete segundo;
+    delete terceiro;
+    delete quarto; //liberar a memoria só por desencargo de consciencia e evitar memory leak
 }
